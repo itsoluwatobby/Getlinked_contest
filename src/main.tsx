0ff70e86e1c6998959e641e6b247a2f9f79672d8
itsoluwatobby <oluwasamuel050@gmail.com>
2023-09-22 13:01:14 +0100

    completed

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import App from './App.tsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import { GetLinkedContextProvider } from './context/GetLinkedContext.tsx'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
